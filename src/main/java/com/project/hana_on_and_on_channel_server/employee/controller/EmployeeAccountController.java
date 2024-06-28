1b6d8de3de34f16418b37f571f6e85fb01276a71
eunjin917 <cgoya01@likelion.org>
2024-06-28 15:41:42 +0900

    refactor/#22 알바생 컨트롤러 분리

---
+++ src/main/java/com/project/hana_on_and_on_channel_server/employee/controller/EmployeeAccountController.java
@@ -0,0 +1,30 @@
+package com.project.hana_on_and_on_channel_server.employee.controller;
+
+import com.project.hana_on_and_on_channel_server.employee.dto.EmployeeAccountRegRequest;
+import com.project.hana_on_and_on_channel_server.employee.dto.EmployeeAccountUpsertRequest;
+import com.project.hana_on_and_on_channel_server.employee.dto.EmployeeAccountUpsertResponse;
+import com.project.hana_on_and_on_channel_server.employee.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/employee")
+public class EmployeeAccountController {
+
+    private final EmployeeService employeeService;
+
+    @PostMapping("/accounts")
+    public ResponseEntity<EmployeeAccountUpsertResponse> registerEmployeeAccount(@AuthenticationPrincipal Long userId, @RequestBody EmployeeAccountRegRequest employeeAccountRegRequest){
+        EmployeeAccountUpsertResponse response = employeeService.registerEmployeeAccount(userId, employeeAccountRegRequest);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/accounts")
+    public ResponseEntity<Void> updateEmployeeAccount(@AuthenticationPrincipal Long userId, @RequestBody EmployeeAccountUpsertRequest employeeAccountUpsertRequest){
+        employeeService.updateEmployeeAccount(userId, employeeAccountUpsertRequest);
+        return ResponseEntity.noContent().build();
+    }
+}
