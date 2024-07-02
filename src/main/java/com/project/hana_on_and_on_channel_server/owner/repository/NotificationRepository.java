be8b67d16c71f63ba72e39d7e36442d14975b9bc
eunjin917 <cgoya01@likelion.org>
2024-07-02 17:17:46 +0900

    feat/#61 공지사항 최신 조회 API 구현

--- src/main/java/com/project/hana_on_and_on_channel_server/owner/repository/NotificationRepository.java
+++ src/main/java/com/project/hana_on_and_on_channel_server/owner/repository/NotificationRepository.java
@@ -5,8 +5,10 @@ import com.project.hana_on_and_on_channel_server.owner.domain.WorkPlace;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     List<Notification> findByWorkPlaceWorkPlaceId(Long workPlaceId);
     List<Notification> findByWorkPlaceOrderByCreatedAtDesc(WorkPlace workPlace);
+    Optional<Notification> findTop1ByWorkPlaceWorkPlaceIdOrderByCreatedAtDesc(Long workPlaceId);
 }
