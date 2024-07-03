cdbcf003817b6becacbabe6ccad2429eb0f9cc12
eunjin917 <cgoya01@likelion.org>
2024-07-03 14:32:11 +0900

    refactor/#81 알바생 fromEntity

--- src/main/java/com/project/hana_on_and_on_channel_server/employee/dto/EmployeeSalaryCalendarGetResponse.java
+++ src/main/java/com/project/hana_on_and_on_channel_server/employee/dto/EmployeeSalaryCalendarGetResponse.java
@@ -2,12 +2,17 @@ package com.project.hana_on_and_on_channel_server.employee.dto;
 
 import com.project.hana_on_and_on_channel_server.attendance.domain.Attendance;
 import com.project.hana_on_and_on_channel_server.attendance.domain.enumType.AttendanceType;
+import com.project.hana_on_and_on_channel_server.attendance.exception.AttendanceNotFoundException;
 import com.project.hana_on_and_on_channel_server.employee.domain.CustomAttendanceMemo;
 import com.project.hana_on_and_on_channel_server.employee.domain.CustomWorkPlace;
 import com.project.hana_on_and_on_channel_server.employee.exception.CustomAttendanceMemoNotFoundException;
 import com.project.hana_on_and_on_channel_server.employee.exception.CustomWorkPlaceNotFoundException;
+import com.project.hana_on_and_on_channel_server.owner.domain.WorkPlace;
+import com.project.hana_on_and_on_channel_server.owner.domain.WorkPlaceEmployee;
 import com.project.hana_on_and_on_channel_server.owner.domain.enumType.ColorType;
 import com.project.hana_on_and_on_channel_server.owner.exception.ColorTypeNotFoundException;
+import com.project.hana_on_and_on_channel_server.owner.exception.WorkPlaceEmployeeNotFoundException;
+import com.project.hana_on_and_on_channel_server.owner.exception.WorkPlaceNotFoundException;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -25,6 +30,37 @@ public record EmployeeSalaryCalendarGetResponse(
         Integer restMinute,
         Integer payment
 ) {
+    // 연결 근무지
+    public static EmployeeSalaryCalendarGetResponse fromEntity(WorkPlaceEmployee workPlaceEmployee, Attendance attendance, Integer payment){
+        if (workPlaceEmployee == null) {
+            throw new WorkPlaceEmployeeNotFoundException();
+        }
+        if (attendance == null) {
+            throw new AttendanceNotFoundException();
+        }
+        WorkPlace workPlace = workPlaceEmployee.getWorkPlace();
+        if (workPlace == null) {
+            throw new WorkPlaceNotFoundException();
+        }
+        ColorType colorType = workPlaceEmployee.getColorType();
+        if (colorType == null) {
+            throw new ColorTypeNotFoundException();
+        }
+        return new EmployeeSalaryCalendarGetResponse(
+                true,
+                attendance.getAttendanceId(),
+                workPlace.getWorkPlaceNm(),
+                colorType.getCode(),
+                attendance.getAttendDate(),
+                attendance.getAttendanceType(),
+                attendance.getAttendanceType() == AttendanceType.REAL ? attendance.getRealStartTime() : attendance.getStartTime(),
+                attendance.getAttendanceType() == AttendanceType.REAL ? attendance.getRealEndTime() : attendance.getEndTime(),
+                attendance.getRestMinute(),
+                payment
+        );
+    }
+
+    // 수동 근무지
     public static EmployeeSalaryCalendarGetResponse fromEntity(CustomWorkPlace customWorkPlace, CustomAttendanceMemo customAttendanceMemo, Integer payment){
         if (customWorkPlace == null) {
             throw new CustomWorkPlaceNotFoundException();
--- src/main/java/com/project/hana_on_and_on_channel_server/owner/repository/WorkPlaceEmployeeRepository.java
+++ src/main/java/com/project/hana_on_and_on_channel_server/owner/repository/WorkPlaceEmployeeRepository.java
@@ -6,9 +6,11 @@ import com.project.hana_on_and_on_channel_server.owner.domain.enumType.EmployeeS
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface WorkPlaceEmployeeRepository extends JpaRepository<WorkPlaceEmployee, Long> {
     List<WorkPlaceEmployee> findByWorkPlaceWorkPlaceId(Long workPlaceId);
-    List<WorkPlaceEmployee> findByWorkPlaceWorkPlaceIdAndEmployeeStatusEquals(Long workPlaceId, EmployeeStatus employeeStatus);
     List<WorkPlaceEmployee> findByEmployee(Employee employee);
+    Optional<WorkPlaceEmployee> findByWorkPlaceWorkPlaceIdAndEmployeeEmployeeId(Long workPlaceId, Long employeeId);
+    List<WorkPlaceEmployee> findByWorkPlaceWorkPlaceIdAndEmployeeStatusEquals(Long workPlaceId, EmployeeStatus employeeStatus);
 }
