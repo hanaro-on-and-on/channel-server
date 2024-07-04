f19716888a1c5a6d6cc03709872f5169390f183c
eunjin917 <cgoya01@likelion.org>
2024-07-04 15:29:35 +0900

    feat/#83 사업자등록번호 확인 API 구현

---
+++ src/main/java/com/project/hana_on_and_on_channel_server/owner/vo/BusinessInfo.java
@@ -0,0 +1,20 @@
+package com.project.hana_on_and_on_channel_server.owner.vo;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class BusinessInfo {
+    private String b_no;
+    private String b_stt;
+    private String b_stt_cd;
+    private String tax_type;
+    private String tax_type_cd;
+    private String end_dt;
+    private String utcc_yn;
+    private String tax_type_change_dt;
+    private String invoice_apply_dt;
+    private String rbf_tax_type;
+    private String rbf_tax_type_cd;
+}
