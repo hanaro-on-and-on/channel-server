ff5413b2cf34f3dd349cb5d9ae33fea7d90040bb
eunjin917 <cgoya01@likelion.org>
2024-07-09 11:44:16 +0900

    fix/#96 jar name 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=channel-server
-JAR_NAME=$REPOSITORY/hana_on_and_on_channel_server-0.0.1-SNAPSHOT-plain.jar
+JAR_NAME=$REPOSITORY/hana_on_and_on_channel_server-0.0.1-SNAPSHOT.jar
 
 echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
