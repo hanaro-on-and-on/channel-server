20faa564f49108d79899bc381a0d0ce49ecc0a97
eunjin917 <cgoya01@likelion.org>
2024-07-09 15:18:28 +0900

    fix/#96 main 수정

--- src/main/java/com/project/hana_on_and_on_channel_server/HanaOnAndOnChannelServerApplication.java
+++ src/main/java/com/project/hana_on_and_on_channel_server/HanaOnAndOnChannelServerApplication.java
@@ -2,6 +2,7 @@ package com.project.hana_on_and_on_channel_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -11,7 +12,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class HanaOnAndOnChannelServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HanaOnAndOnChannelServerApplication.class, args);
+		SpringApplication application = new SpringApplication(HanaOnAndOnChannelServerApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
 
 }
